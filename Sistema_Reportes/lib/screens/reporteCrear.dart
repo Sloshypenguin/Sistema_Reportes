0fbad43ec0071c7ce1d1680316bd0863e4b1ede1
AlexxxMikuUwU <xdavid927@gmail.com>
2025-05-29 15:29:06 -0600

    feat(Crear Screen)

---
+++ Sistema_Reportes/lib/screens/reporteCrear.dart
@@ -0,0 +1,4 @@
+import '../models/reporteViewModel.dart';
+import '../services/reporteService.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
